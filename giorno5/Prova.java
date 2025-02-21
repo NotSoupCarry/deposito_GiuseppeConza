dc30a0151be91b694d9b6b65e7df1241415ca362
Giuseppe Conza <giuseppeforce36@gmail.com>
2025-02-21 11:29:45 +0100

    Create Prova.java

---
+++ giorno5/Prova.java
@@ -0,0 +1,11 @@
+public class Prova {
+    static public void main(String[] args){
+        System.out.println("hello world \f");
+        System.out.println("hello world \r");
+        System.out.println("\nhello worldssss ");
+
+        String vuoto = "          ";
+        System.out.println(vuoto.trim().isEmpty());
+
+    }
+}
